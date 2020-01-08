691d35fb67d1b08a557b87678633644d83c46111
elaitala <elaitala@gmail.com>
2020-01-08 09:30:38 -0800

    Figuring out animation of road

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,15 @@
+console.log('The game is running...')
+
 // Build and scroll the road
 // Scroll down and stitch image together to create road movine effect
 // Set speed to static. Allow arrow up/down buttons to speed up/slow down scroll
 
 // Step 1: Start the game
-$('button').on('click', () => {
-  console.log('Game starts...');
-  setUpRound();
-  setTimer();
-});
+// $('button').on('click', () => {
+//   console.log('Game starts...');
+//   setUpRound();
+//   setTimer();
+// });
 
 
 // Build the motorcycle
@@ -20,14 +22,14 @@ $('button').on('click', () => {
 // Add rectangles that move in parallax with the road
 // Number of cars to navigate is an array
 
-const createCars = (numberOfCars) => {
-  const $cars = $('.cars');
-  for (let i = 0; i < numberOfCars; i++) {
-     const $car = $('<div class="car"/>')
-     $car.css('background-color', applyRandomColor());
-     $cars.append($car)
-  }
-}
+// const createCars = (numberOfCars) => {
+//   const $cars = $('.cars');
+//   for (let i = 0; i < numberOfCars; i++) {
+//      const $car = $('<div class="car"/>')
+//      $car.css('background-color', applyRandomColor());
+//      $cars.append($car)
+//   }
+// }
 
 
 // Add object collision
